6394d19774a25db9a7e2793cd50978d409430a32
Patrick Okwe <okwepatrick@gmail.com>
2021-07-24 00:19:27 +0200

    How to check for an item in a list

--- start.py
+++ start.py
@@ -1,1 +1,15 @@
 # let us write some codes that we can understand
+# what is a list in python it is simply a collection of data that we can read.
+
+list = ["Patrick", "Lucky", "Many", True]
+# how do i check if an item is inside of my list?
+list2 = list.copy()
+print(list2)
+
+# clear() can actually clear the contents of a list
+
+numbers = [1, 2, 4, 5, 7]
+times = [u*u for u in numbers]
+
+print(numbers)
+print(times)
