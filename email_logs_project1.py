02a4406d89a4f83add45f1202381fe515462aabf
patito4779 <okwepatrick@gamil.com>
2022-04-29 11:37:52 +0200

    Added the username and password as variables to parsed in when running the code as .stdin parameters

--- email_logs_project1.py
+++ email_logs_project1.py
@@ -1,4 +1,13 @@
 """
 Get all the keys contents of an email messages and log it out to an external text file
 """
+import imaplib
+import email
+from email.header import decode_header
+import webbrowser
+import os
+import sys
+
+username = sys.argv[1]
+password = sys.argv[2]
 
